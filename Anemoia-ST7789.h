f46fba44f82a6421ba537d50b46e79539fb38cec
Shim Manaloto <shimmanaloto@gmail.com>
2026-01-25 00:10:20 +0800

    build: define TOUCH_CS to silence TFT_eSPI warning

--- Anemoia-ST7789.h
+++ Anemoia-ST7789.h
@@ -17,6 +17,8 @@
 #define TFT_DC     2
 #define TFT_RST    -1
 
+#define TOUCH_CS -1
+
 
 #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
 #define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
