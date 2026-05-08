42390406c6698b7259fe928d6b8a106742b087f3
Shim Manaloto <shimmanaloto@gmail.com>
2026-05-08 13:40:13 +0800

    fix(mapper): reduce banks to alleviate RAM

--- src/core/mappers/mapper069.h
+++ src/core/mappers/mapper069.h
@@ -4,7 +4,7 @@
 #include "../mapper.h"
 
 #define MAPPER069_NUM_PRG_BANKS_8K 17
-#define MAPPER069_NUM_CHR_BANKS_1K 26
+#define MAPPER069_NUM_CHR_BANKS_1K 24
 
 Mapper createMapper069(uint8_t PRG_banks, uint8_t CHR_banks, ROMBackend backend, Cartridge* cart);
 
