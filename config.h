62e53e494359901e0ff52171eacce6566476c7d6
Shim Manaloto <shimmanaloto@gmail.com>
2026-08-06 11:44:24 +0800

    fix(config): ensure VIDEO_STANDARD can be overridden

--- config.h
+++ config.h
@@ -3,8 +3,10 @@
 
 // Uncomment if you are using composite video output instead of a TFT display
 // #define COMPOSITE_VIDEO
-#define VIDEO_STANDARD 1 // 0 = PAL, 1 = NTSC
-#define AUDIO_PIN      18
+#ifndef VIDEO_STANDARD
+    #define VIDEO_STANDARD 1 // 0 = PAL, 1 = NTSC
+#endif
+#define AUDIO_PIN 18
 
 // #define CHEAP_YELLOW_DISPLAY_CONF // Uncomment this line if using the CYD
 // #define MODULE_BASED_PCB_CONF // Uncomment this line if using the module PCB
